de01b91c851e3330d0e1f442474d0393876d1e37
ZakRab <Zakomaha@gmail.com>
2022-08-18 10:02:16 -0500

    intial commit

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import second from ''
+const Main = () => {
+
+  return (
+    <div>
+    <h1>welcome to drawing game</h1>
+    <label htmlFor="lobbykey">Enter the Lobby key</label>
+    <input id="lobbykey" type="text" />
+    </div>
+  )
+}
+
+export default Main
---
+++ src/context/LobbyContext.jsx
@@ -0,0 +1,79 @@
+import React, { useCallback, useState } from "react";
+import axios from "axios";
+export const UserContext = React.createContext(null);
+
+export function lobbyProvider(props) {
+  const [lobby, setLobby] = useState(null);
+
+  const logIn = useCallback(
+    async (username, password) => {
+      try {
+        const res = await axios.post("/api/users/login", {
+          username,
+          password,
+        });
+        console.log(res);
+        if (res.data.success) {
+          setLoggedInUser(res.data.data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    [setLoggedInUser]
+  );
+
+  const logOut = useCallback(async () => {
+    try {
+      const res = await axios.get("/api/user/logout");
+      if (res.data.success) {
+        setLoggedInUser(null);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, [setLoggedInUser]);
+
+  const register = useCallback(async (username, password) => {
+    try {
+      const res = await axios.put("/api/users/register", {
+        username,
+        password,
+      });
+      console.log(res);
+      if (res.data.success) {
+        console.log("registered!");
+        return true;
+      }
+      return false;
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  const verify = useCallback(async () => {
+    try {
+      const res = await axios.get("/api/users/verify");
+      if (res.data.success) {
+        setLoggedInUser(res.data.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  return (
+    <UserContext.Provider
+      value={{
+        verify,
+        loggedInUser,
+        setLoggedInUser,
+        logIn,
+        logOut,
+        register,
+      }}
+    >
+      {props.children}
+    </UserContext.Provider>
+  );
+}
