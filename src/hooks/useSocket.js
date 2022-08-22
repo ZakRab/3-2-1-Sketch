79b1f12b6a37ec6ec551adfe91cb880272653c66
ZakRab <Zakomaha@gmail.com>
2022-08-22 14:40:51 -0500

    clears commented model

--- src/hooks/useSocket.js
+++ src/hooks/useSocket.js
@@ -39,38 +39,6 @@ const useSocket = (lobbyKey) => {
       }
     });
   }, [lobbyKey]);
-
-  //   // useEffect(() => {
-  //   //   socketRef.current = socketIoClient("http://localhost:8080", {
-  //   //     query: { displayName, lobbyKey },
-  //   //   });
-  //   //   socketRef.current.on("user connect", ({ displayName }) => {
-  //   //     let newMsg = {
-  //   //       displayName: "SERVER",
-  //   //       body: `${displayName} has joined the room`,
-  //   //       color: "#00ff00",
-  //   //     };
-  //   //   });
-  //   //   socketRef.current.on("user disconnect", ({ displayName }) => {
-  //   //     let newMsg = {
-  //   //       displayName: "SERVER",
-  //   //       body: `${displayName} has left the room`,
-  //   //       color: "#00ff00",
-  //   //     };
-  //   //   });
-  //   //   socketRef.current.on("message", (msg) => {
-  //   //     setMessages((curr) => [...curr, msg]);
-  //   //   });
-  //   //   return () => socketRef.current.disconnect();
-  //   // }, []);
-
-  //   // const sendMessage = useCallback(
-  //   //   (body) => {
-  //   //     socketRef.current.emit("message", { color, displayName, body });
-  //   //   },
-  //   //   [color, displayName]
-  //   // );
-  //   // return { messages, sendMessage };
   return { players };
 };
 export default useSocket;
