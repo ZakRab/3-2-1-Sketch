d44abc17e2085a2f852cf599742684ffb7088c19
ZakRab <107758804+ZakRab@users.noreply.github.com>
2023-05-25 23:18:41 +0200

    fixes footer

--- src/components/Lobby.jsx
+++ src/components/Lobby.jsx
@@ -54,6 +54,8 @@ const Lobby = () => {
       {isSketching && <Sketch SendSketch={SendSketch}></Sketch>}
       {isVoting && <Vote ToResults={ToResults} SendVote={SendVote}></Vote>}
       {isResults && <Results ResetRound={ResetRound} rounds={rounds}></Results>}
+      <div className="footer-filler">
+      </div>
       <footer className="d-flex top-border space-between">
         <h2 className="margin-top-small">-{activePlayer.displayName}-</h2>
         {(isSketching || isResults || isVoting) && (
